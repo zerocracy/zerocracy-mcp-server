04a05b41ceecdad6eef097c1d2323847fcadb7f1
Yegor Bugayenko <yegor256@gmail.com>
2025-04-29 18:11:30 +0300

    #4 test

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 Zerocracy
 // SPDX-License-Identifier: MIT
 
+import { Readable, Writable } from "node:stream";
+import { JSONRPCMessage } from "@modelcontextprotocol/sdk/types.js";
+import { ReadBuffer, serializeMessage } from "@modelcontextprotocol/sdk/shared/stdio.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { describe, expect, test } from '@jest/globals';
 import { server } from '../src/server';
 import { FakeTransport } from './fake-transport';
@@ -10,5 +14,32 @@ describe('server', () => {
     const transport = new FakeTransport();
     await server.connect(transport);
     expect(server.isConnected()).toBe(true);
+  });
+
+  test('returns simple answer from baza', async () => {
+    const stdin = new Readable(
+      {
+        read: () => serializeMessage(
+          {
+            jsonrpc: "2.0",
+            id: 1,
+            method: "ping",
+          }
+        )
+      }
+    );
+    const buffer = new ReadBuffer();
+    const stdout = new Writable(
+      {
+        write(chunk, encoding, callback) {
+          buffer.append(chunk);
+          console.log('boom!');
+          callback();
+        }
+      }
+    );
+    await server.connect(new StdioServerTransport(stdin, stdout));
+    expect(buffer.readMessage()).not.toBeNull();
+    await server.close();
   });
 });
