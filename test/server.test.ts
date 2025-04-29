8a19722d97c11cfdc8e32e5adcc6ab2b2b5b14a2
Yegor Bugayenko <yegor256@gmail.com>
2025-04-29 18:23:51 +0300

    #4 passes

--- test/server.test.ts
+++ test/server.test.ts
@@ -16,30 +16,30 @@ describe('server', () => {
     expect(server.isConnected()).toBe(true);
   });
 
-  test('returns simple answer from baza', async () => {
-    const stdin = new Readable(
-      {
-        read: () => serializeMessage(
-          {
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'tools/list',
-          }
-        )
+  test('lists all tools', async () => {
+    const serializedMessage = serializeMessage({
+      jsonrpc: "2.0" as const,
+      id: 1,
+      method: 'tools/list',
+    });
+    const stdin = new Readable({
+      read() {
+        this.push(serializedMessage);
+        this.push(null);
       }
-    );
+    });
     const buffer = new ReadBuffer();
-    const stdout = new Writable(
-      {
-        write(chunk, encoding, callback) {
-          buffer.append(chunk);
-          console.log('boom!');
-          callback();
-        }
+    const stdout = new Writable({
+      write(chunk, encoding, callback) {
+        buffer.append(chunk);
+        callback();
       }
-    );
+    });
     await server.connect(new StdioServerTransport(stdin, stdout));
+    await new Promise(resolve => setTimeout(resolve, 500));
     await server.close();
-    expect(buffer.readMessage()).not.toBeNull();
+    const answer = buffer.readMessage();
+    expect(answer).not.toBeNull();
+    // expect(answer['result']['tools'].length).toBeGreaterThan(0);
   });
 });
